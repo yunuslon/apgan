d5ad406354749617a1be427f51d448de041f6ed7
Muhammad Yunus <muhammad.yunus@edi-indonesia.co.id>
2022-09-23 09:43:32 +0700

    hide bottom tab bar with listner

--- src/components/BottomNavigator/index.js
+++ src/components/BottomNavigator/index.js
@@ -1,46 +1,104 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Keyboard, Platform, StyleSheet, View} from 'react-native';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 import {colors} from '../../utils';
 import TabItem from './TabItem';
 
 const BottomNavigator = ({state, descriptors, navigation}) => {
-  return (
-    <View style={styles.container}>
-      {state.routes.map((route, index) => {
-        const {options} = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+  const [visible, setVisible] = React.useState(true);
 
-        const isFocused = state.index === index;
+  React.useEffect(() => {
+    let keyboardEventListeners;
+    if (Platform.OS === 'android') {
+      keyboardEventListeners = [
+        Keyboard.addListener('keyboardDidShow', () => setVisible(false)),
+        Keyboard.addListener('keyboardDidHide', () => setVisible(true)),
+      ];
+    }
+    return () => {
+      if (Platform.OS === 'android') {
+        keyboardEventListeners && keyboardEventListeners.forEach(eventListener => eventListener.remove());
+      }
+    };
+  }, []);
+  const render = () => {
+    if (Platform.OS === 'ios') {
+      return (
+        <View style={styles.container}>
+          {state.routes.map((route, index) => {
+            const {options} = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.name;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+            const isFocused = state.index === index;
 
-          if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name);
-          }
-        };
+            const onPress = () => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true,
+              });
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(route.name);
+              }
+            };
 
-        return <TabItem key={index} title={label} active={isFocused} onPress={onPress} onLongPress={onLongPress} />;
-      })}
-    </View>
-  );
+            const onLongPress = () => {
+              navigation.emit({
+                type: 'tabLongPress',
+                target: route.key,
+              });
+            };
+
+            return <TabItem key={index} title={label} active={isFocused} onPress={onPress} onLongPress={onLongPress} />;
+          })}
+        </View>
+      );
+    }
+    if (!visible) return null;
+    return (
+      <View style={styles.container}>
+        {state.routes.map((route, index) => {
+          const {options} = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+              ? options.title
+              : route.name;
+
+          const isFocused = state.index === index;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return <TabItem key={index} title={label} active={isFocused} onPress={onPress} onLongPress={onLongPress} />;
+        })}
+      </View>
+    );
+  };
+  return render();
 };
 
 export default BottomNavigator;
